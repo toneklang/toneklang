5871b05f8164af6c2b9cbeb193357fb780077313
toneklang <71411071+toneklang@users.noreply.github.com>
2023-10-08 01:45:06 +0200

    Update music.js

--- music.js
+++ music.js
@@ -8,13 +8,26 @@ const divider = 60;
 const colls = width / divider
 const rows = colls;
 
+function orangeSquare() {
+
+    context.save();
+    context.fillStyle = "white";
+    context.fillRect((width / 4) - 13, (height / 4) - 13, width, height); 
+    context.restore();
+
+    context.save();
+    context.fillStyle = "orange";
+    context.fillRect(width / 4, height / 4, width, height);
+    context.restore();
+};
 
 
 // Control frame rate by specifying a target frame rate (e.g., 60 frames per second).
-const targetFrameRate = 6;
+const targetFrameRate = 10;
 const frameInterval = 1000 / targetFrameRate;
 
 let lastFrameTime = 0;
+
 
 function drawMe(timestamp) {
     const elapsedTime = timestamp - lastFrameTime;
@@ -22,8 +35,6 @@ function drawMe(timestamp) {
     if (elapsedTime >= frameInterval) {  
 
         context.clearRect(0, 0, canvas.width, canvas.height);
-
-      
 
         for (let i = 0; i < divider; i++) {
             for (let j = 0; j < divider; j++){
@@ -36,17 +47,6 @@ function drawMe(timestamp) {
 
                 const rectColor = "rgb(" + rCol + "," + gCol + "," + bCol + ")";
                 const colorRect = rectColor;
-
-                context.save();
-                context.fillStyle = "white";
-                context.fillRect((width / 4) - 13, (height / 4) - 13, width, height); 
-                context.restore();
-        
-                context.save();
-                context.fillStyle = "orange";
-                context.fillRect(width / 4, height / 4, width, height);
-                context.restore();
-                
                 
                 context.save();
                 context.translate(0, 0);
@@ -54,7 +54,7 @@ function drawMe(timestamp) {
                 context.fillRect(dx, dy, rows, colls);
                 context.clearRect(dx + 2, dy + 2, rows * 0.75, colls * 0.75); 
                 context.translate(0.02 * i, 0.02 * j);
-                context.fillStyle = "white"; 
+                context.fillStyle = "#0D0D0D"; 
                 context.fillRect(dx + j * 2, dy - Math.floor(Math.random() * 4) * j, rows * Math.random()*0.7, colls * Math.random()); 
                 context.strokeRect = "green";
                 context.restore();
@@ -65,8 +65,12 @@ function drawMe(timestamp) {
         // Update the last frame time.
         lastFrameTime = timestamp;
 
+        orangeSquare();
     }
     requestAnimationFrame(drawMe);
 }
 
+
+
 drawMe();
+
